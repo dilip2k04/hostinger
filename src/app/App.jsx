a2cb671cdbdff49170d57141d0e154dbcd1543cb
vickydecodes <darklegendvs@gmail.com>
2025-02-06 09:45:25 +0530

    update

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,24 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "./App.css";
 import Hero from "../pages/Hero/Hero";
-<<<<<<< HEAD
 import AboutUs from "../pages/AboutUs/aboutus";
-=======
-import Aboutus from "../pages/about us/aboutus";
 import OurProgram from "../pages/OurProgram/OurProgram";
->>>>>>> 8212d0d8b636f9f2632b9f9d4e25a3de834aacfe
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero/>
-<<<<<<< HEAD
       <AboutUs/>
-=======
-      <Aboutus/>
       <OurProgram/>
->>>>>>> 8212d0d8b636f9f2632b9f9d4e25a3de834aacfe
       <Footer />
 
     </>
