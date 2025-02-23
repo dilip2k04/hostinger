07412f02a217b5b2569df1bc5b8498b76ea3bc36
vickydecodes <darklegendvs@gmail.com>
2025-02-23 14:17:07 +0530

    update

--- src/pages/OurProgram/OurProgram.jsx
+++ src/pages/OurProgram/OurProgram.jsx
@@ -6,8 +6,9 @@ import BroadCard from "./components/BroadCard/BroadCard";
 import Testimonial from "./components/Testimonial/Testimonial";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
+import AboutUs from "../AboutUs/AboutUs";
 
-export default function OurProgram() {
+export default function OurProgram({is_main_page}) {
   const [testimonials, setTestimonials] = useState([]);
   useEffect(() => {
     const fetch_testimonials = async () => {
@@ -83,7 +84,6 @@ export default function OurProgram() {
     },
   ];
 
-
   const location = useLocation();
 
   useEffect(() => {
@@ -100,7 +100,137 @@ export default function OurProgram() {
       }
     }
   }, [location]);
-  
+
+  const OurProgramTab1 = () => {
+    return (
+      <div className="ourprogram" id="our_program_section">
+        <h5 className="ourprogram_title ">
+          <i>Our Programs </i>
+        </h5>
+
+        <div className="ourprogram_tab row g-0">
+          <div className="col-md-7" id="ourprogram_img_Tab">
+            <img
+              className="ourprogram_img"
+              src="/imgs/our program 1.jpg"
+              alt="image"
+            />
+          </div>
+          <div className="col-md-5 p-2 d-flex justify-content-center align-items-center flex-column">
+            {" "}
+            <h4 className="ourprogram_content">
+              We offer one-on-one tutoring sessions tailored to each student's
+              unique learning style and pace, ensuring effective understanding
+              and retention of concepts.
+            </h4>
+            <a
+              className="ourprogram_button btn hover-border-primary"
+              href="#contact_us"
+            >
+              Book a Demo
+            </a>
+            <hr />
+          </div>
+        </div>
+        <Tab title="Grade Specific Online Tuition" is_title={true} />
+
+        <div className="row g-0 mb-4" id="Courses">
+          <div className="col-md-6 p-2 mb-3 d-flex flex-column justify-content-center align-items-center">
+            <h3 className="grade_title">Grade 3 - 5</h3>
+            <div className="card text-bg-dark grade_card">
+              <img src="/imgs/gradec2.png" className="card-img" alt="..." />
+              <div className="card-img-overlay overlay-text">
+                <p className="card-text p-3 pt-5 fs-5">
+                  Strong focus on foundational skills in Mathematics, Science,
+                  and English. Activities that stimulate curiosity and
+                  independent problem-solving
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 p-2 d-flex flex-column justify-content-center align-items-center">
+            <h3 className="grade_title">Grade 6 - 9</h3>
+            <div className="card text-bg-dark grade_card">
+              <img src="/imgs/gradec1.png" className="card-img" alt="..." />
+              <div className="card-img-overlay overlay-text">
+                <p className="card-text p-3 pt-5 fs-5">
+                  Advanced concepts in core subjects to prepare students for
+                  academic challenges. Comprehensive guidance to build critical
+                  thinking and analytical skills.
+                </p>
+              </div>
+            </div>
+            {/* <div className="position-relative">
+         <img
+            src="/imgs/gradec1.png"
+            className="grade_img shadow-lg"
+            alt="image"
+          />
+          <p className="position-absolute top-50 start-50">Hii bro</p>
+         </div> */}
+          </div>
+        </div>
+        <Tab title="Subjects We Cover" is_title={true} />
+
+        <div className="all_sub p-5 pt-0 row d-flex g-0">
+          {subject_data.map((sub, idx) => {
+            return (
+              <div
+                key={idx}
+                className="col-md-4 d-flex justify-content-center align-items-center"
+              >
+                <Card title={sub.title} img={sub.img} link={sub.link} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  const OurProgramTab2 = () => {
+    return (
+      <>
+        <Tab title="Teaching Methodology" is_title={true} />
+        <div
+          className="teaching_container w-100 d-flex  flex-column justify-content-center align-items-center"
+          id="Teaching"
+        >
+          {teaching_data.map((d, idx) => {
+            return (
+              <BroadCard
+                key={idx}
+                title={d.title}
+                content={d.content}
+                img={d.img}
+                is_right_pic={d.is_right_pic}
+              />
+            );
+          })}
+        </div>
+        <Tab title="Testimonials" is_title={true} />
+        <div className="testimonial-container ">
+          <div className="testimonial-wrapper row g-1">
+            {testimonials.map((testimonial, idx) => (
+              <div className="testimonial-item col-md-3" key={idx}>
+                <Testimonial testimonial={testimonial} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  if (is_main_page) {
+    return (
+      <>
+        <OurProgramTab1/>
+        <AboutUs />
+        <OurProgramTab2 />
+      </>
+    );
+  }
 
   return (
     <div className="ourprogram" id="our_program_section">
@@ -141,8 +271,9 @@ export default function OurProgram() {
             <img src="/imgs/gradec2.png" className="card-img" alt="..." />
             <div className="card-img-overlay overlay-text">
               <p className="card-text p-3 pt-5 fs-5">
-              Strong focus on foundational skills in Mathematics, Science, and English.
-              Activities that stimulate curiosity and independent problem-solving
+                Strong focus on foundational skills in Mathematics, Science, and
+                English. Activities that stimulate curiosity and independent
+                problem-solving
               </p>
             </div>
           </div>
@@ -153,9 +284,9 @@ export default function OurProgram() {
             <img src="/imgs/gradec1.png" className="card-img" alt="..." />
             <div className="card-img-overlay overlay-text">
               <p className="card-text p-3 pt-5 fs-5">
-              Advanced concepts in core subjects to prepare students for academic challenges.
-Comprehensive guidance to build critical thinking and analytical skills.
-
+                Advanced concepts in core subjects to prepare students for
+                academic challenges. Comprehensive guidance to build critical
+                thinking and analytical skills.
               </p>
             </div>
           </div>
@@ -202,15 +333,14 @@ Comprehensive guidance to build critical thinking and analytical skills.
       </div>
       <Tab title="Testimonials" is_title={true} />
       <div className="testimonial-container ">
-  <div className="testimonial-wrapper row g-1">
-    {testimonials.map((testimonial, idx) => (
-      <div className="testimonial-item col-md-3" key={idx}>
-        <Testimonial testimonial={testimonial} />
+        <div className="testimonial-wrapper row g-1">
+          {testimonials.map((testimonial, idx) => (
+            <div className="testimonial-item col-md-3" key={idx}>
+              <Testimonial testimonial={testimonial} />
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-  </div>
-</div>
-
     </div>
   );
 }
