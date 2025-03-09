8f36629af71651cbf426e97d7b98f7c52653cbed
rajkaran <rajkaran1505@gmail.com>
2025-03-09 20:09:59 +0530

    Mq

--- src/pages/Home/components/VerticalCards/VerticalCards.jsx
+++ src/pages/Home/components/VerticalCards/VerticalCards.jsx
@@ -7,7 +7,7 @@ export default function VerticalCards() {
         <div className="card cross_card" style={{width: '60%', height:'227px'}}>
             <div className="card-body-2 p-0 d-flex justify-content-center align-items-center flex-column">
               <div className="cross_card_content text-center text-muted p-3">
-              <img src="/imgs/cross_card1.png" className="vertical_card_img" alt="" style={{marginBottom:'0px'}}/>
+              <img src="/imgs/cross_card1.png" className="vertical_card_img" alt="" style={{marginBottom:'10px'}}/>
                 <h3 className='mb-3'style={{color:'black'}}>Elite Mentorship</h3>
                 <span  style={{fontSize: '16px'}}>Mentoring from IIT-IIM Alumni Founders</span>              
               </div>
@@ -20,7 +20,7 @@ export default function VerticalCards() {
           </div>
           <div className="card cross_card"  style={{width: '60%', height:'227px'}}>
             <div className="card-body-2 d-flex justify-content-center align-items-center flex-column">
-            <img src="/imgs/cross_card2.png"  className="vertical_card_img" alt="" style={{marginBottom:'0px'}}/>
+            <img src="/imgs/cross_card2.png"  className="vertical_card_img" alt="" style={{marginBottom:'10px'}}/>
 
                 <h3 className='mb-3'>One-on-One <br />Sessions</h3>
               <div className="cross_card_content text-center text-muted p-3">
@@ -32,7 +32,7 @@ export default function VerticalCards() {
           </div>
           <div className="card cross_card"  style={{width: '60%' , height:'227px'}}>
             <div className="card-body-2  d-flex justify-content-center align-items-center flex-column">
-            <img src="/imgs/cross_card3.png" className="vertical_card_img" alt="" style={{marginBottom:'0px' , height:'50px'}} />
+            <img src="/imgs/cross_card3.png" className="vertical_card_img" alt="" style={{marginBottom:'10px' , height:'50px'}} />
 
                 <h3 className='mb-3'>Personalized study plans</h3>
               <div className="content cross_card_content text-center text-muted p-3">
@@ -44,7 +44,7 @@ export default function VerticalCards() {
           </div>
           <div className="card cross_card" style={{width: '60%',height:'227px'}}>
             <div className="card-body-2 d-flex justify-content-center align-items-center flex-column">
-            <img src="/imgs/cross_card4.png" className="vertical_card_img" alt="" style={{marginBottom:'0px'}}/>
+            <img src="/imgs/cross_card4.png" className="vertical_card_img" alt="" style={{marginBottom:'10px'}}/>
 
                 <h3 className='mb-3'>Certified Mentors</h3>
               <div className="cross_card_content text-center text-muted p-3">
